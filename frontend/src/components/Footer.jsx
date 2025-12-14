0abbd142cb8ceab5978b1d7e2feb275863ebe400
talithadifa <talithadaliladifa@gmail.com>
2025-12-15 00:41:38 +0700

    hapus file frontend di branch frontend-talitha

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="w-full bg-blue-900 text-white text-sm py-4 mt-10 text-center">
-      © 2025 SIMATRO Universitas Lampung. All Rights Reserved.
-    </footer>
-  );
-}
